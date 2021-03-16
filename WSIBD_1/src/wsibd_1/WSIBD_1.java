fd8d3405989f5f4426ae3525b7043ee66720f042
haniatulkhoiriyah <haniatul122@gmail.com>
2021-03-16 16:37:14 +0700

    Create WSIBD_1.java

---
+++ WSIBD_1/src/wsibd_1/WSIBD_1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wsibd_1;
+
+/**
+ *
+ * @author ACER
+ */
+public class WSIBD_1 {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("world !");
+        
+        System.out.print("hello");
+        System.out.print("world !");
+    }
+    
+}
