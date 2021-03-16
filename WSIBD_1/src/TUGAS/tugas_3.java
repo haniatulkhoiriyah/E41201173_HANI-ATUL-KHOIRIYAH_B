42ae7b5145723c446e7e554b354d4ec65c10f8ae
haniatulkhoiriyah <haniatul122@gmail.com>
2021-03-16 16:38:03 +0700

    Create tugas_3.java

---
+++ WSIBD_1/src/TUGAS/tugas_3.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TUGAS;
+
+/**
+ *
+ * @author ACER
+ */
+public class tugas_3 {
+    public static void main(String[] args) {
+        boolean gameOver = false;
+        int students = 50, classes = 3;
+        double sales_tax;
+        short number;
+        
+        int 2beOrNot2be;
+        float price index;
+        double lastYear'sPrice;
+        long class;
+    }
+}
