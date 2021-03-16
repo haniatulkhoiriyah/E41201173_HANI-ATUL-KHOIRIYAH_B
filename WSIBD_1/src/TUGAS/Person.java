b6ccd89585504a4e25b540316560dd0f535943d6
haniatulkhoiriyah <haniatul122@gmail.com>
2021-03-16 16:37:43 +0700

    Create Person.java

---
+++ WSIBD_1/src/TUGAS/Person.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TUGAS;
+
+/**
+ *
+ * @author ACER
+ */
+public class Person {
+    public static void main(String[] args) {
+        String fName = "Lisa";
+        String lName = "Palombo";
+        String stuID = "123456789";
+        String stuStatus = "Active";
+                
+        System.out.println("Student Name : " + fName + " " + lName);
+        System.out.println("Student ID : " + stuID );
+        System.out.println("Student status : " + stuStatus);
+        
+    }
+}
