a8660408ec99d70b903b96c26181478f51c22058
haniatulkhoiriyah <haniatul122@gmail.com>
2021-03-16 16:37:24 +0700

    Create latihan_2.java

---
+++ WSIBD_1/src/wsibd_1/latihan_2.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wsibd_1;
+
+/**
+ *
+ * @author ACER
+ */
+public class latihan_2 {
+    public static void main(String[] args) {
+        String nama = "Indonesia";
+        char abjad = 'a';
+        int a = 6;
+        double b = 6.9;
+        
+        System.out.println (nama);
+        System.out.println (a);
+    }
+}
