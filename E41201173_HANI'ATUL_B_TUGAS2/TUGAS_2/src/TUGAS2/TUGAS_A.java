e73669a069161b1b87f5648eb161287716dd53c1
haniatulkhoiriyah <haniatul122@gmail.com>
2021-03-23 12:25:59 +0700

    Create TUGAS_A.java

---
+++ E41201173_HANI'ATUL_B_TUGAS2/TUGAS_2/src/TUGAS2/TUGAS_A.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TUGAS2;
+
+/**
+ *
+ * @author ACER
+ */
+public class TUGAS_A {
+    public static void main(String[] args) {
+        String[] nama = {"Hani", "Atul", "Khoiriyah"};
+        
+        for(int i=0; i<nama.length; i++) {
+            System.out.print(nama[i]);
+            System.out.print(" ");
+        }
+    }
+}
