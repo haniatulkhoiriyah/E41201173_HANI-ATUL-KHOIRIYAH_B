d629a639aa9dfb7361c0c50a8cf9ad8b0f3a828f
haniatulkhoiriyah <haniatul122@gmail.com>
2021-03-30 20:24:08 +0700

    Create LATIHAN_1.java

---
+++ E41201173_HANI'ATUL_B_TUGAS3/TUGAS_3/src/LATIHAN/LATIHAN_1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package LATIHAN;
+
+/**
+ *
+ * @author ACER
+ */
+public class LATIHAN_1 {
+    public static void main (String[] args) {
+    int i = 0;
+    
+    while (i <= 10) {
+        System.out.println("Perulangan ke-" + i);
+    i++;
+        }
+    }
+}
