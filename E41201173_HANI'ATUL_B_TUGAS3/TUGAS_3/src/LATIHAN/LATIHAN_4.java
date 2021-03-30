e8f3e014005b99391d03e33af1bff11f2061dd33
haniatulkhoiriyah <haniatul122@gmail.com>
2021-03-30 20:25:15 +0700

    Create LATIHAN_4.java

---
+++ E41201173_HANI'ATUL_B_TUGAS3/TUGAS_3/src/LATIHAN/LATIHAN_4.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package LATIHAN;
+
+/**
+ *
+ * @author ACER
+ */
+public class LATIHAN_4 {
+    public static void main(String[] args) {
+        int angka[] = {3, 1, 42, 24, 12};
+        
+        for(int x : angka) {
+            System.out.print(x + " ");
+        }
+    }
+}
